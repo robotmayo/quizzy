67d985cefd11f314c053b46168d3e6f2df7754dc
robotmayo <robotmayo@gmail.com>
2013-07-09 21:47:52 -0400

    I need to learn to stick to the one change rule...

--- js/quizzy.js
+++ js/quizzy.js
@@ -8,8 +8,8 @@ choices : ["yes","no","Only on Tuesdays"],
 answer : 2
 },
 {question: "How much wood could a woodchuck chuck if a woodchuck could chuck wood?",
-choices : ["11","4231","yes"],
-answer : 2
+choices : ["11","4231","yes","BUTTERED TOAST"],
+answer : 3
 }
 ];
 function getRandom(min, max) {
@@ -29,11 +29,54 @@ function shuffleArray(array){
 var quizzy = (function(){
     var _quizzy = {};
     var _questions;
+    var _quizContainer;
+    var _answerElements;
+    var _frag;
+    var _title;
+    var _inputWrap;
+
+    _quizzy.currentQuestion;
+    _quizzy.questionCount = -1;
+    _quizzy.totalQuestions;
     _quizzy.init = function(){
+        // Duplicates the questions into this internal array
         _questions = questions.slice();
-        console.log(_questions);
         shuffleArray(_questions);
-        console.log(_questions);
+        _quizContainer = document.getElementById("quizzy");
+        if(_quizContainer == null){
+            throw new Error("Couldn't find quizzy starting element. Aborting mission!");
+        }
+        _quizzy.setupElements();
+        _quizzy.nextQuestion();
+    }
+    _quizzy.setupElements = function(){
+        _frag = document.createDocumentFragment();
+        _title = document.createElement('h2');
+        _title.id = "quizzy-title";
+
+        _inputWrap = document.createElement('div');
+        _inputWrap.id = "quizzy-input-wrap";
+        _frag.appendChild(_title);
+        _frag.appendChild(_inputWrap);
+        _quizContainer.appendChild(_frag);
+        while(_frag.lastChild){_frag.removeChild(_frag.lastChild)}
+    }
+    _quizzy.nextQuestion = function(){
+        _quizzy.questionCount++;
+        var radio;
+        var label;
+        _title.innerHTML = _questions[_quizzy.questionCount].question;
+        for(var i = 0; i <= _questions[_quizzy.questionCount].choices.length-1; i++){
+            label = document.createElement("label");
+            radio = document.createElement("input");
+            radio.type = 'radio';
+            radio.name = 'quizzy-radio';
+            radio.value = _questions[_quizzy.questionCount].choices[i];
+            label.appendChild(radio);
+            label.innerHTML += " "+_questions[_quizzy.questionCount].choices[i];
+            _frag.appendChild(label);
+        }
+        _inputWrap.appendChild(_frag);
     }
 
     
