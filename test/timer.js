218e444a02d31636d62de67aa73d4272197ec4ae
robotmayo <robotmayo@gmail.com>
2013-07-13 13:30:10 -0400

    Updated to newest version.

--- test/timer.js
+++ test/timer.js
@@ -1,3 +1,23 @@
+// Constants
+    var _hours = 1000 * 60 * 60;
+    var _minutes = 1000 * 60;
+    var _seconds = 1000;
+
+Timer = function(tick,handler,start){
+    this.handler = handler || null;
+    this.tick = tick || 20;
+    this.elapsed = 0;
+    this.last = 0;
+    this.clock = 0;
+    this.minutes = 0;
+    this.seconds = 0;
+    this.milliseconds = 0;
+    console.log(this.tick);
+
+    if(start){
+        this.start();
+    }
+}
 Timer.prototype.start = function() {
     console.log(this.tick);
     this.last = Date.now();
@@ -12,22 +32,31 @@ Timer.prototype.stop = function() {
 Timer.prototype.update = function(){
     this.elapsed = Date.now() - this.last;
     this.clock += this.elapsed;
-    if(this.clock > 1000){
-        this.seconds += Math.floor(this.clock / 1000);
-        this.clock = 0;
-    }
-    if(this.seconds > 60){
-        this.minutes += Math.floor(this.seconds / 60);
-    }
-    if(this.minutes > 60){
-        this.hours += Math.floor(this.minutes / 60);
-    }
+    this.milliseconds += elapsed;
+
+    this.seconds += Math.floor(this.milliseconds / _seconds);
+    this.minutes += Math.floor(this.seconds / 60);
+    this.hours = Math.floor(this.clock / _hours);
+    if(this.seconds > 60) this.seconds = 0;
+    if(this.minutes > 60) this.
     this.last = Date.now();
-    this.printTime();
-}
+};
 Timer.prototype.getTime = function() {
-    return this.sTime;
+    return {
+        hours:this.hours,
+        minutes:this.minutes,
+        seconds:this.seconds,
+        milliseconds:this.clock
+    };
 };
 Timer.prototype.printTime = function() {
-    console.log(this.seconds);
+    var time = this.getTime();
+    var hr;
+    var min;
+    var sec;
+    hr = time.hours;
+    min = time.minutes < 10 ? "0"+time.minutes : time.minutes;
+    sec = time.seconds < 10 ? "0"+time.seconds : time.seconds;
+    console.log(hr+":"+min+":"+sec);
+
 };
