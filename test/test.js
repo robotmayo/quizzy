5406a0989d19bb508edd52043af48f0f325be234
robotmayo <robotmayo@gmail.com>
2013-07-13 18:56:52 -0400

    Testing input creation

--- test/test.js
+++ test/test.js
@@ -2,3 +2,8 @@ var ll = new LinkedList();
 for(var i = 0; i < 100; i++){
     ll.push(i);
 }
+
+var d = quizzy.createInput('radio',[24,235,53262,6226,235626,23]);
+for(var i = 0; i < d.length; i++){
+    console.log(d[i].value);
+}
