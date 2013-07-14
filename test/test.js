0e9685a9652c1c6303949ec17c454be10ba8f463
robotmayo <robotmayo@gmail.com>
2013-07-13 23:38:47 -0400

    Testing the new createInput method.

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ for(var i = 0; i < 100; i++){
 }
 
 var d = quizzy.createInput('radio',[24,235,53262,6226,235626,23]);
-for(var i = 0; i < d.length; i++){
-    console.log(d[i].value);
-}
-quizzy.init();
+console.log(d[0].value);
+d = quizzy.createInput('radio',10);
+console.log(d);
+//quizzy.init();
