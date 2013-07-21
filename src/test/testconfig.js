0d566e5543e2de4a4b63d6f024efb093cd80e8c9
robotmayo <robotmayo@gmail.com>
2013-07-21 17:26:54 -0400

    Added a config to test time taken over the quiz

--- src/test/testconfig.js
+++ src/test/testconfig.js
@@ -71,3 +71,21 @@ var testConfigTwo ={
         }
     ]
 };
+var timerConfig = {
+    questions : [
+        {   question: "How many masks can you obtain in Legend of Zelda : Majora's Mask",
+            choices : [12,17,24,29],
+            answer : 2
+        },
+        {   question: "How much time do you have to save the world in Legend of Zelda : Majora's Mask",
+            choices : ["13 Days","2 Weeks","1 Month","3 Days"],
+            answer : 3
+        },
+        {   question: "What does the Bunny Hood do?",
+            choices : ["Grants you a movement boost","Allows you to talk to small rodents","Turns you into a bunny"],
+            answer : 0
+        }
+    ],
+    quizTimer : true,
+    trackQuizTime : true
+}
